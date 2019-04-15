14e1cdc9eea570b87b54162dcbf8b0bce65343cb
Kayode Hadilou ADJE <hadilou@users.noreply.github.com>
2019-04-16 02:40:07 +0300

    adapt to my dataset

--- dataset.py
+++ dataset.py
@@ -6,24 +6,30 @@ import os
 import random
 import argparse
 
+train_path='/train'
+validation_path='./validation'
+train_annotation_path='./train_annotation--'
+validation_annotation='./validation_annotation--'
+	
 def _int64_feature(value):
 	return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 def _bytes_feature(value):
 	return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-def load_image(path,index,load_label=True,tif=False):
-	img_path = os.path.join(path,'%d.png'%index)
-	if tif:
-		img_path = os.path.join(path,'%d.tif'%index)
+def load_image(path,period,load_label=True):
+	img_path = os.path.join(path,'.jpg')
+	
 	image = cv2.imread(img_path,flags=cv2.IMREAD_UNCHANGED)
 	image = image[:,:,0:3]
 	image = np.uint8(image)
-
+	if period == train:
+		root=train_path
+	else:
+		root=validation_path
 	if load_label:
-		label_path = os.path.join(path,'%d_class.png'%index)
-		if tif:
-			label_path = os.path.join(path,'%d_class.tif'%index)
+		label_path=os.path.splitext(root)[0]
+		label_path=os.path.join(label_path,'%.png')
 		label = cv2.imread(label_path)
 		label = label[:,:,0]
 		label = np.uint8(label)
@@ -74,12 +80,18 @@ def save_image(sub_image, sub_label, writer,augment=False):
 		writer.write(example.SerializeToString())
 
 def main(args):
+	train_path='/train'
+	validation_path='./validation'
+	train_annotation_path='./train_annotation'
+	validation_annotation='./validation_annotation'
+	
 	dataset_name='BDCI'
 	dataset_name2 = 'dataset'
 	dataset_name3 = 'BDCI-semi'
 	dataset_path='./BDCI2017-jiage'
 	dataset_path2 = './dataset'
 	dataset_path3 = './BDCI2017-jiage-Semi'
+	
 	period = args.period
 	TFRecord_path='./TFRecord'
 	
@@ -89,20 +101,11 @@ def main(args):
 	patch_size = 256
 
 	if period=='train':
-		data_size = 2
-		sample_size = 1024
-		path = os.path.join(dataset_path,period)
-		for i in range(1,data_size+1):
-			image, label = load_image(path,i)
-			row = image.shape[0]
-			col = image.shape[1]
-			num = np.int64((row/sample_size+1)*(col/sample_size+1))*2
-			for j in range(0,num):
-				sub_image,sub_label = random_patch(image,label,sample_size)
-				sub_image=cv2.resize(sub_image,(patch_size,patch_size),interpolation=cv2.INTER_CUBIC)
-				sub_label=cv2.resize(sub_label,(patch_size,patch_size),interpolation=cv2.INTER_NEAREST)
-				save_image(sub_image,sub_label,writer,augment=True)
-				print('NO.%d patch in %s-%s-%d is saving...'%(j, dataset_name, period,i))
+		for image_path in (os.listdir(train_path)):
+			label_path=os.path.splitext(image_path)[0]
+			image,label= load_image(image_path,'train')
+			save_image(image,label,writer,augment=True)
+			print('Done Preparing traing dataset')
 
 		# data_size = 5
 		# sample_size = 256
@@ -120,54 +123,18 @@ def main(args):
 		# 		save_image(sub_image,sub_label,writer,augment=True)
 		# 		print('NO.%d patch in %s-%s-%d is saving...'%(j, dataset_name2, period,i))
 
-		data_size = 3
-		sample_size = 1024
-		path = os.path.join(dataset_path3,period)
-		for i in range(1,data_size+1):
-			image, label = load_image(path,i)
-			copy_label = label.copy()
-			copy_label[label==2]=3
-			copy_label[label==3]=4
-			copy_label[label==4]=2
-			label = copy_label.copy()
-			row = image.shape[0]
-			col = image.shape[1]
-			num = np.int64((row/sample_size+1)*(col/sample_size+1))*4
-			for j in range(0,num):
-				sub_image,sub_label = random_patch(image,label,sample_size)
-				sub_image=cv2.resize(sub_image,(patch_size,patch_size),interpolation=cv2.INTER_CUBIC)
-				sub_label=cv2.resize(sub_label,(patch_size,patch_size),interpolation=cv2.INTER_NEAREST)
-				save_image(sub_image,sub_label,writer,augment=True)
-				print('NO.%d patch in %s-%s-%d is saving...'%(j, dataset_name3, period,i))
-		writer.close()
+	if period=='validation':
+		for image_path in (os.listdir(validation_path)):
+		label_path=os.path.splitext(image_path)[0]
+		image,label= load_image(image_path,'validation')
+		save_image(image,label,writer,augment=True)
+		print('Done Preparing validation dataset')
+	writer.close()
 
-	else:
-		if period=='valid':
-			data_size=2
-			path = os.path.join(dataset_path,'train')
-			for i in range(1,data_size+1):
-				image, label=load_image(path,i)
-				save_image(image,label,writer)
-				print('BDCI NO.%d valid sample is saving...'%i)
-
-			data_size=5
-			path = os.path.join(dataset_path2,'train')
-			for i in range(1,data_size+1):
-				image, label=load_image(path,i,tif=True)
-				label[label==5]=2
-				save_image(image,label,writer)
-				print('dataset NO.%d valid sample is saving...'%i)
-		else:
-			data_size=3
-			path = os.path.join(dataset_path,'test')
-			for i in range(i,data_size+1):
-				image = load_image(path,i)
-				save_image(image,[],writer)
-				print('NO.%d test sample is saving...'%i)
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(usage="it's usage tip.", description="help info.")
-	parser.add_argument("--period", choices=['train', 'valid', 'test'], default="train", help="period")
+	parser.add_argument("--period", choices=['train', 'validation'], default="train", help="period")
 	args = parser.parse_args()
 	main(args)
